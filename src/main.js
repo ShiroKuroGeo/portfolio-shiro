4d3757f64026f8d737a64e259e9e759d6ccdcb98
ShiroGeo <inocgeorgealfeser@gmail.com>
2026-01-26 12:37:44 +0800

    added analytics vercel

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import VueSweetalert2 from 'vue-sweetalert2'
 import 'sweetalert2/dist/sweetalert2.min.css'
+import { Analytics } from '@vercel/analytics'
 
 const app = createApp(App)
 app.use(VueSweetalert2)
+Analytics.init()
 app.mount('#app')
